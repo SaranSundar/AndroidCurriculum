48fe0b1c9f6f975af6b09b2182bf2c8d20af4c56
Saran Sundararajan <saran@sarans-mbp.utdallas.edu>
2018-08-29 18:40:15 -0500

    Scraper

--- ChineseNovelReader/app/src/main/java/com/sszg/chinesenovelreader/GridItemAdapter.java
+++ ChineseNovelReader/app/src/main/java/com/sszg/chinesenovelreader/GridItemAdapter.java
@@ -1,25 +1,89 @@
 package com.sszg.chinesenovelreader;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class GridItemAdapter {
+public class GridItemAdapter extends RecyclerView.Adapter<GridItemAdapter.GridViewHolder> {
     private ArrayList<Book> books;
+    private Context context;
+    private ItemClickListener mClickListener;
+
+    public GridItemAdapter() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public Book getBook(int i) {
+        return books.get(i);
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public GridViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        context = viewGroup.getContext();
+        View rowView = LayoutInflater.from(context).inflate(R.layout.grid_item, viewGroup, false);
+        return new GridViewHolder(rowView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GridViewHolder gridViewHolder, int i) {
+        if (i % 2 == 0) {
+            gridViewHolder.coverPicture.setImageResource(R.drawable.hidden_marriage);
+        } else {
+            gridViewHolder.coverPicture.setImageResource(R.drawable.ghost_emperor);
+        }
+        gridViewHolder.title.setText(books.get(i).getTitle());
+        String chpUn = i + " unread";
+        gridViewHolder.chaptersUnread.setText(chpUn);
+    }
+
+    @Override
+    public int getItemCount() {
+        return books.size();
+    }
 
 
-    public class GridViewHolder extends RecyclerView.ViewHolder {
+    public class GridViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private ImageView coverPicture;
         private CheckBox checkBox;
         private TextView title, chaptersUnread;
 
         public GridViewHolder(@NonNull View itemView) {
             super(itemView);
+            coverPicture = itemView.findViewById(R.id.cover_picture);
+            checkBox = itemView.findViewById(R.id.check_box);
+            title = itemView.findViewById(R.id.title);
+            chaptersUnread = itemView.findViewById(R.id.chapters_unread);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) {
+                mClickListener.onItemClick(view, getAdapterPosition());
+            }
         }
     }
 }
--- ChineseNovelReader/app/src/main/java/com/sszg/chinesenovelreader/MainActivity.java
+++ ChineseNovelReader/app/src/main/java/com/sszg/chinesenovelreader/MainActivity.java
@@ -1,13 +1,101 @@
 package com.sszg.chinesenovelreader;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements GridItemAdapter.ItemClickListener {
+
+    private RecyclerView recyclerView;
+    private GridItemAdapter gridAdapter;
+    private static ProgressDialog progressDialog;
+    private static String wuxiaWorldURL = "https://www.wuxiaworld.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupGrid();
+        populateGrid();
+        wuxiaWorldURL += "novel/a-will-eternal/awe-chapter-1";
+    }
+
+    private class WuxiaWorld extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setTitle("Wuxia World");
+            progressDialog.setMessage("Loading Book Catalog...");
+            progressDialog.setIndeterminate(false);
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            progressDialog.dismiss();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Document page = Jsoup.connect(wuxiaWorldURL).get();
+//                Elements table = page.select("table[class=table table-novels]");
+//                Elements titles = table.select("span[class=title]");
+//                for (Element e : titles) {
+//                    System.out.println(e.text());
+//                }
+                Elements story = page.select("div[class=fr-view]");
+                String chp1 = story.text();
+                System.out.println(chp1);
+                System.out.println(story.text());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    public void populateGrid() {
+        for (int i = 0; i < 10; i++) {
+            Book book1 = new Book("", "Hidden Marriage");
+            Book book2 = new Book("", "Paradise Land");
+            Book book3 = new Book("", "Tree Farms & Fun");
+            gridAdapter.addBook(book1);
+            gridAdapter.addBook(book2);
+            gridAdapter.addBook(book3);
+        }
+        gridAdapter.notifyDataSetChanged();
+    }
+
+    public void setupGrid() {
+        recyclerView = findViewById(R.id.recycler_view);
+        int numOfColumns = 3;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, numOfColumns));
+        gridAdapter = new GridItemAdapter();
+        gridAdapter.setClickListener(this);
+        recyclerView.setAdapter(gridAdapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Book book = gridAdapter.getBook(position);
+        Toast.makeText(this, "Title: " + book.getTitle(), Toast.LENGTH_SHORT).show();
+        new WuxiaWorld().execute();
     }
 }
