f8599c1d8e0c46576a602d1f5b1b33472cf58b3f
Saran Sundar <saran@Sarans-iMac.local>
2018-09-03 21:59:03 -0500

    Frame

--- ChineseNovelReader/app/src/main/java/com/sszg/chinesenovelreader/BookReaderActivity.java
+++ ChineseNovelReader/app/src/main/java/com/sszg/chinesenovelreader/BookReaderActivity.java
@@ -1,13 +1,32 @@
 package com.sszg.chinesenovelreader;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 public class BookReaderActivity extends AppCompatActivity {
+
+    private RelativeLayout bookSettings;
+    private LinearLayout chapterBody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_reader);
+        chapterBody = findViewById(R.id.chapter_body);
+        bookSettings = findViewById(R.id.book_settings);
+    }
+
+    public void clickScreen(View v) {
+        Toast.makeText(this, "WORKING", Toast.LENGTH_SHORT).show();
+        if (bookSettings.getVisibility() == View.INVISIBLE) {
+            bookSettings.setVisibility(View.VISIBLE);
+        } else {
+            bookSettings.setVisibility(View.INVISIBLE);
+        }
     }
 }
